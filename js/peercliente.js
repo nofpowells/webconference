6945bb4ce2ca231e7e3113182671eb659099118d
nofpowells <elfqibr@gmail.com>
2015-06-17 02:08:10 -0300

    primeiro commit

---
+++ js/peercliente.js
@@ -0,0 +1,100 @@
+// Compatibilidade dos browsers
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var mediaOptions = { audio: true, video: true};
+var tamanhoVideo =" width:280px";
+var tamanhoVideo1 = "width:auto";
+
+// Objeto peer.
+var peer = new Peer({host:'192.168.1.102', port: 9000, debug: true, config: {'iceServers': [
+      { url: 'stun:stun.l.google.com:19302' }
+    ]}});
+
+var userList = [];
+// Busca todos os ids criados no servidor.
+peer.listAllPeers(function(list){
+    for(var i = 0;i < list.length;i++)
+    {
+        userList.push(list[i]);
+    }
+});
+
+peer.on('open', function(){
+  $('#meu-id').append("SEU ID: " + peer.id);
+});
+
+// recebendo chamada
+peer.on('call', function(call){
+  // responde a chamada automaticamente
+  call.answer(window.localStream);
+  CriarChamada(call);
+});
+
+peer.on('error', function(err){
+  alert(err.message);
+});
+
+// Funções de click - Botões
+$(function(){
+    
+    
+    $('#chamar').click(function(){
+    // Inicia Chamada
+    var call;
+    if(userList.length == 0){
+        call = peer.call($('#chamar-id').val(), window.localStream);
+        CriarChamada(call);
+    }else
+    {
+        for(var i = 0;i < userList.length;i++){
+			call = peer.call(userList[i].toString(), window.localStream);
+            CriarChamada(call);
+		}         
+    }   
+   
+  });
+  //Finaliza chamada
+  $('#desligar-chamada').click(function(){
+      window.existingCall.close();
+  });
+
+  // Inicia o processo da chamada
+  detectaMedias();
+});
+
+function detectaMedias() 
+{
+  // captura o video e o audio
+  navigator.getUserMedia(mediaOptions, function(stream)
+  {
+    // Aqui você insere o local da sua cam.
+    $('#video-deireito').prop('src', URL.createObjectURL(stream));
+
+    window.localStream = stream;
+  },function(){ $('#erro').show(); });
+};
+
+function CriarChamada(call) 
+{  
+  // Se ja existe uma chamada, adiciona na barra lateral direita dos participantes
+  if (window.existingCall) 
+  {
+    call.on('stream', function(remoteStream){
+      $('#participantes').append("<video id='" + call.peer + "'style='" + tamanhoVideo + "' autoplay></video><p>'"+call.peer+"'</p>");
+      $('#' + call.peer).prop('src', URL.createObjectURL(remoteStream));
+    });
+    
+  }
+  else
+  {
+      // Se for o primeiro a entrar é direcionado para o centro da tela
+    call.on('stream', function(stream){
+      $('#video-principal').append("<video id='" + call.peer + "'style='" + tamanhoVideo1 + "' autoplay></video><p>'"+call.peer+"'</p>");
+      $('#' + call.peer).prop('src', URL.createObjectURL(stream));
+    });     
+
+  }
+  
+  // UI stuff
+  window.existingCall = call;
+}
